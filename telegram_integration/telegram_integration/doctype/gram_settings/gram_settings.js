65e2f962524918fab74ef9c1b5445b70d52fca4e
Ahmed Al-farran <afarran1992@gmail.com>
2023-01-18 13:43:10 +0300

    test: rename doctype

---
+++ telegram_integration/telegram_integration/doctype/gram_settings/gram_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Ahmed Al-farran and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Gram Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
