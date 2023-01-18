f801c55feb8f5990c09d5ed6d40a77ebb3197573
Ahmed Al-farran <afarran1992@gmail.com>
2023-01-18 13:13:31 +0300

    first commit

---
+++ telegram_integration/telegram_integration/doctype/telegram_notification/telegram_notification.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Ahmed Al-farran and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Telegram Notification', {
+	// refresh: function(frm) {
+
+	// }
+});
