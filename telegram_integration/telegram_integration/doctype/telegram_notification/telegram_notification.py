281482661d69218089a88d894efb9df39cc0889b
Ahmed Al-farran <afarran1992@gmail.com>
2023-01-18 21:05:05 +0300

    fix: get_dynamic_recipients method

--- telegram_integration/telegram_integration/doctype/telegram_notification/telegram_notification.py
+++ telegram_integration/telegram_integration/doctype/telegram_notification/telegram_notification.py
@@ -3,7 +3,8 @@
 
 from __future__ import unicode_literals
 import frappe
-import json, os
+import json
+import os
 from frappe import _
 from frappe.model.document import Document
 
@@ -24,271 +25,293 @@ from telegram_integration.telegram_integration.doctype.telegram_settings.telegra
 
 
 class TelegramNotification(Document):
-    def onload(self):
-        """load message"""
-        if self.is_standard:
-            self.message = self.get_template()
+	def onload(self):
+		"""load message"""
+		if self.is_standard:
+			self.message = self.get_template()
 
-    def autoname(self):
-        if not self.name:
-            self.name = self.subject
+	def autoname(self):
+		if not self.name:
+			self.name = self.subject
 
-    def validate(self):
-        validate_template(self.subject)
-        validate_template(self.message)
+	def validate(self):
+		validate_template(self.subject)
+		validate_template(self.message)
 
-        if self.event in ("Days Before", "Days After") and not self.date_changed:
-            frappe.throw(_("Please specify which date field must be checked"))
+		if self.event in ("Days Before", "Days After") and not self.date_changed:
+			frappe.throw(_("Please specify which date field must be checked"))
 
-        if self.event == "Value Change" and not self.value_changed:
-            frappe.throw(_("Please specify which value field must be checked"))
+		if self.event == "Value Change" and not self.value_changed:
+			frappe.throw(_("Please specify which value field must be checked"))
 
-        self.validate_forbidden_types()
-        self.validate_condition()
-        self.validate_standard()
-        frappe.cache().hdel("tel_notifications", self.document_type)
+		self.validate_forbidden_types()
+		self.validate_condition()
+		self.validate_standard()
+		frappe.cache().hdel("tel_notifications", self.document_type)
 
-    def on_update(self):
-        path = export_module_json(self, self.is_standard, self.module)
-        if path:
-            # js
-            if not os.path.exists(path + ".md") and not os.path.exists(path + ".html"):
-                with open(path + ".md", "w") as f:
-                    f.write(self.message)
+	def on_update(self):
+		path = export_module_json(self, self.is_standard, self.module)
+		if path:
+			# js
+			if not os.path.exists(path + ".md") and not os.path.exists(path + ".html"):
+				with open(path + ".md", "w") as f:
+					f.write(self.message)
 
-            # py
-            if not os.path.exists(path + ".py"):
-                with open(path + ".py", "w") as f:
-                    f.write(
-                        """from __future__ import unicode_literals
+			# py
+			if not os.path.exists(path + ".py"):
+				with open(path + ".py", "w") as f:
+					f.write(
+						"""
+						from __future__ import unicode_literals
+						import frappe
 
-import frappe
+						def get_context(context):
+						# do your magic here
+						pass
+						"""
+					)
 
-def get_context(context):
-	# do your magic here
-	pass
-"""
-                    )
 
-    def validate_standard(self):
-        if self.is_standard and not frappe.conf.developer_mode:
-            frappe.throw(
-                _(
-                    "Cannot edit Standard Notification. To edit, please disable this and duplicate it"
-                )
-            )
+	def validate_standard(self):
+		if self.is_standard and not frappe.conf.developer_mode:
+			frappe.throw(
+				_(
+					"Cannot edit Standard Notification. To edit, please disable this and duplicate it"
+				)
+			)
 
-    def validate_condition(self):
-        temp_doc = frappe.new_doc(self.document_type)
-        if self.condition:
-            try:
-                frappe.safe_eval(self.condition, None, get_context(temp_doc))
-            except Exception:
-                frappe.throw(_("The Condition '{0}' is invalid").format(self.condition))
 
-    def validate_forbidden_types(self):
-        forbidden_document_types = ("Email Queue",)
-        if (
-            self.document_type in forbidden_document_types
-            or frappe.get_meta(self.document_type).istable
-        ):
-            # currently notifications don't work on child tables as events are not fired for each record of child table
+	def validate_condition(self):
+		temp_doc = frappe.new_doc(self.document_type)
+		if self.condition:
+			try:
+				frappe.safe_eval(self.condition, None, get_context(temp_doc))
+			except Exception:
+				frappe.throw(
+					_("The Condition '{0}' is invalid").format(self.condition))
 
-            frappe.throw(
-                _("Cannot set Notification on Document Type {0}").format(
-                    self.document_type
-                )
-            )
 
-    def get_documents_for_today(self):
-        """get list of documents that will be triggered today"""
-        docs = []
+	def validate_forbidden_types(self):
+		forbidden_document_types = ("Email Queue",)
+		if (
+			self.document_type in forbidden_document_types
+			or frappe.get_meta(self.document_type).istable
+		):
+			# currently notifications don't work on child tables as events are not fired for each record of child table
 
-        diff_days = self.days_in_advance
-        if self.event == "Days After":
-            diff_days = -diff_days
+			frappe.throw(
+				_("Cannot set Notification on Document Type {0}").format(
+					self.document_type
+				)
+			)
 
-        reference_date = add_to_date(nowdate(), days=diff_days)
-        reference_date_start = reference_date + " 00:00:00.000000"
-        reference_date_end = reference_date + " 23:59:59.000000"
 
-        doc_list = frappe.get_all(
-            self.document_type,
-            fields="name",
-            filters=[
-                {self.date_changed: (">=", reference_date_start)},
-                {self.date_changed: ("<=", reference_date_end)},
-            ],
-        )
+	def get_documents_for_today(self):
+		"""get list of documents that will be triggered today"""
+		docs = []
 
-        for d in doc_list:
-            doc = frappe.get_doc(self.document_type, d.name)
+		diff_days = self.days_in_advance
+		if self.event == "Days After":
+			diff_days = -diff_days
 
-            if self.condition and not frappe.safe_eval(
-                self.condition, None, get_context(doc)
-            ):
-                continue
+		reference_date = add_to_date(nowdate(), days=diff_days)
+		reference_date_start = reference_date + " 00:00:00.000000"
+		reference_date_end = reference_date + " 23:59:59.000000"
 
-            docs.append(doc)
+		doc_list = frappe.get_all(
+			self.document_type,
+			fields="name",
+			filters=[
+				{self.date_changed: (">=", reference_date_start)},
+				{self.date_changed: ("<=", reference_date_end)},
+			],
+		)
 
-        return docs
+		for d in doc_list:
+			doc = frappe.get_doc(self.document_type, d.name)
 
-    def send(self, doc):
-        """Build recipients and send Notification"""
+			if self.condition and not frappe.safe_eval(
+				self.condition, None, get_context(doc)
+			):
+				continue
 
-        context = get_context(doc)
-        context = {"doc": doc, "alert": self, "comments": None}
-        if doc.get("_comments"):
-            context["comments"] = json.loads(doc.get("_comments"))
+			docs.append(doc)
 
-        if self.is_standard:
-            self.load_standard_properties(context)
+		return docs
 
-        if self.channel == "Telegram":
-            self.send_a_telegram_msg(doc, context)
 
-        if self.set_property_after_alert:
-            allow_update = True
-            if (
-                doc.docstatus == 1
-                and not doc.meta.get_field(
-                    self.set_property_after_alert
-                ).allow_on_submit
-            ):
-                allow_update = False
+	def send(self, doc):
+		"""Build recipients and send Notification"""
 
-            if allow_update:
-                frappe.db.set_value(
-                    doc.doctype,
-                    doc.name,
-                    self.set_property_after_alert,
-                    self.property_value,
-                    update_modified=False,
-                )
-                doc.set(self.set_property_after_alert, self.property_value)
+		context = get_context(doc)
+		context = {"doc": doc, "alert": self, "comments": None}
+		if doc.get("_comments"):
+			context["comments"] = json.loads(doc.get("_comments"))
 
-    def get_dynamic_recipients(self, doc):
-        recipients_telegram_user_list = []
-        field_names = ["Customer", "Supplier", "Student", "Employee", "User"]
-        if self.dynamic_recipients:
-            fields = get_doc_fields(self.document_type)
-            for d in fields:
-                party = d.get("field_options")
-                if not party:
-                    if (
-                        d.get("field_get_value")
-                        and doc.get(d["field_get_value"]) in field_names
-                    ):
-                        party = doc.get(d["field_get_value"])
-                    else:
-                        break
+		if self.is_standard:
+			self.load_standard_properties(context)
 
-                filters = {
-                        "party": party,
-                        "telegram_user": doc.get(d["fieldname"]),
-                    }
-                telegram_user_list = frappe.get_all(
-                    "Telegram User Settings",
-                    filters=filters,
-                    fields=["name", "telegram_settings", "telegram_user"],
-                )
-                for i in telegram_user_list:
-                    recipients_telegram_user_list.append(i.name)
-        return recipients_telegram_user_list
+		if self.channel == "Telegram":
+			self.send_a_telegram_msg(doc, context)
 
-    def send_a_telegram_msg(self, doc, context):
-        recipients_telegram_user_list = []
-        if self.telegram_user:
-            recipients_telegram_user_list.append(self.telegram_user)
-        recipients_telegram_user_list.extend(self.get_dynamic_recipients(doc))
-        space = "\n" * 2
-        message = frappe.render_template(self.subject, context) + space
-        message = message + frappe.render_template(self.message, context)
-        attachment = self.get_attachment(doc)
-        for telegram_user in recipients_telegram_user_list:
-            send_to_telegram(
-                telegram_user=telegram_user,
-                message=message,
-                reference_doctype=doc.doctype,
-                reference_name=doc.name,
-                attachment=attachment,
-            )
+		if self.set_property_after_alert:
+			allow_update = True
+			if (
+				doc.docstatus == 1
+				and not doc.meta.get_field(
+					self.set_property_after_alert
+				).allow_on_submit
+			):
+				allow_update = False
 
-            doc.message_notification = message
-            doc.from_user = frappe.session.user
-            doc.party_type = frappe.get_value(
-                "Telegram User Settings", telegram_user, "party"
-            )
-            doc.to_party = frappe.get_value(
-                "Telegram User Settings", telegram_user, "telegram_user"
-            )
-            creat_extra_notification_log(doc)
+			if allow_update:
+				frappe.db.set_value(
+					doc.doctype,
+					doc.name,
+					self.set_property_after_alert,
+					self.property_value,
+					update_modified=False,
+				)
+				doc.set(self.set_property_after_alert, self.property_value)
 
-    def get_attachment(self, doc):
-        """ check print settings are attach the pdf """
-        if not self.attach_print:
-            return None
 
-        print_settings = frappe.get_doc("Print Settings", "Print Settings")
-        if (doc.docstatus == 0 and not print_settings.allow_print_for_draft) or (
-            doc.docstatus == 2 and not print_settings.allow_print_for_cancelled
-        ):
+	def get_dynamic_recipients(self, doc):
+		"""
+		if 'Dynamic Recipients' is checked means that notification will be sent
+		to all 'Party' field in the DocType you created notification for if any
+		'Party' field has value.
+		"""
+		recipients_telegram_user_list = []
+		field_names = ["Customer", "Supplier", "Student", "Employee", "User"]
+		
+		if self.dynamic_recipients:
+			fields = get_doc_fields(self.document_type)
+			for d in fields:
+				filters = {}
+				telegram_user_list = []
+				if doc.get(d["fieldname"]):
+					party = d.get("field_options")
+					if not party:
+						if (
+							d.get("field_get_value")
+							and doc.get(d["field_get_value"]) in field_names
+						):
+							party = doc.get(d["field_get_value"])
+						else:
+							break
+					# if doc.get(d["fieldname"]):
+					filters = {
+						"party": party,
+						"telegram_user": doc.get(d["fieldname"]),
+					}
+					# if filters:
+					telegram_user_list = frappe.get_all(
+						"Telegram User Settings",
+						filters=filters,
+						fields=["name", "telegram_settings", "telegram_user"],
+					)
+					for i in telegram_user_list:
+						recipients_telegram_user_list.append(i.name)
+		return recipients_telegram_user_list
 
-            # ignoring attachment as draft and cancelled documents are not allowed to print
-            status = "Draft" if doc.docstatus == 0 else "Cancelled"
-            frappe.throw(
-                _(
-                    """Not allowed to attach {0} document,
+
+	def send_a_telegram_msg(self, doc, context):
+		recipients_telegram_user_list = []
+		if self.telegram_user:
+			recipients_telegram_user_list.append(self.telegram_user)
+		recipients_telegram_user_list.extend(self.get_dynamic_recipients(doc))
+		space = "\n" * 2
+		message = frappe.render_template(self.subject, context) + space
+		message = message + frappe.render_template(self.message, context)
+		attachment = self.get_attachment(doc)
+		for telegram_user in recipients_telegram_user_list:
+			send_to_telegram(
+				telegram_user=telegram_user,
+				message=message,
+				reference_doctype=doc.doctype,
+				reference_name=doc.name,
+				attachment=attachment,
+			)
+
+			doc.message_notification = message
+			doc.from_user = frappe.session.user
+			doc.party_type = frappe.get_value(
+				"Telegram User Settings", telegram_user, "party"
+			)
+			doc.to_party = frappe.get_value(
+				"Telegram User Settings", telegram_user, "telegram_user"
+			)
+			creat_extra_notification_log(doc)
+
+
+	def get_attachment(self, doc):
+		""" check print settings are attach the pdf """
+		if not self.attach_print:
+			return None
+
+		print_settings = frappe.get_doc("Print Settings", "Print Settings")
+		if (doc.docstatus == 0 and not print_settings.allow_print_for_draft) or (
+			doc.docstatus == 2 and not print_settings.allow_print_for_cancelled
+		):
+
+			# ignoring attachment as draft and cancelled documents are not allowed to print
+			status = "Draft" if doc.docstatus == 0 else "Cancelled"
+			frappe.throw(
+				_(
+					"""Not allowed to attach {0} document,
 				please enable Allow Print For {0} in Print Settings""".format(
-                        status
-                    )
-                ),
-                title=_("Error in Notification"),
-            )
-        else:
-            return [
-                {
-                    "print_format_attachment": 1,
-                    "doctype": doc.doctype,
-                    "name": doc.name,
-                    "print_format": self.print_format,
-                    "print_letterhead": print_settings.with_letterhead,
-                    "lang": frappe.db.get_value(
-                        "Print Format", self.print_format, "default_print_language"
-                    )
-                    if self.print_format
-                    else "en",
-                }
-            ]
+						status
+					)
+				),
+				title=_("Error in Notification"),
+			)
+		else:
+			return [
+				{
+					"print_format_attachment": 1,
+					"doctype": doc.doctype,
+					"name": doc.name,
+					"print_format": self.print_format,
+					"print_letterhead": print_settings.with_letterhead,
+					"lang": frappe.db.get_value(
+						"Print Format", self.print_format, "default_print_language"
+					)
+					if self.print_format
+					else "en",
+				}
+			]
 
-    def get_template(self):
-        module = get_doc_module(self.module, self.doctype, self.name)
 
-        def load_template(extn):
-            template = ""
-            template_path = os.path.join(
-                os.path.dirname(module.__file__), frappe.scrub(self.name) + extn
-            )
-            if os.path.exists(template_path):
-                with open(template_path, "r") as f:
-                    template = f.read()
-            return template
+	def get_template(self):
+		module = get_doc_module(self.module, self.doctype, self.name)
 
-        return load_template(".html") or load_template(".md")
+		def load_template(extn):
+			template = ""
+			template_path = os.path.join(
+				os.path.dirname(module.__file__), frappe.scrub(
+					self.name) + extn
+			)
+			if os.path.exists(template_path):
+				with open(template_path, "r") as f:
+					template = f.read()
+			return template
 
-    def load_standard_properties(self, context):
-        """load templates and run get_context"""
-        module = get_doc_module(self.module, self.doctype, self.name)
-        if module:
-            if hasattr(module, "get_context"):
-                out = module.get_context(context)
-                if out:
-                    context.update(out)
+		return load_template(".html") or load_template(".md")
 
-        self.message = self.get_template()
 
-        if not is_html(self.message):
-            self.message = frappe.utils.md_to_html(self.message)
+	def load_standard_properties(self, context):
+		"""load templates and run get_context"""
+		module = get_doc_module(self.module, self.doctype, self.name)
+		if module:
+			if hasattr(module, "get_context"):
+				out = module.get_context(context)
+				if out:
+					context.update(out)
+
+		self.message = self.get_template()
+
+		if not is_html(self.message):
+			self.message = frappe.utils.md_to_html(self.message)
 
 
 @frappe.whitelist()
@@ -317,8 +340,8 @@ def run_telegram_notifications(doc, method):
     def _evaluate_alert(alert):
         if not alert.name in doc.flags.tel_notifications_executed:
             if frappe.db.exists("Telegram Notification", alert.name):
-               evaluate_alert(doc, alert.name, alert.event)
-               doc.flags.tel_notifications_executed.append(alert.name)
+                evaluate_alert(doc, alert.name, alert.event)
+                doc.flags.tel_notifications_executed.append(alert.name)
 
     event_map = {
         "on_update": "Save",
@@ -360,7 +383,8 @@ def trigger_notifications(doc, method=None):
     if method == "daily":
         doc_list = frappe.get_all(
             "Telegram Notification",
-            filters={"event": ("in", ("Days Before", "Days After")), "enabled": 1},
+            filters={
+                "event": ("in", ("Days Before", "Days After")), "enabled": 1},
         )
         for d in doc_list:
             alert = frappe.get_doc("Telegram Notification", d.name)
@@ -417,7 +441,8 @@ def evaluate_alert(doc, alert, event):
             ).format(alert)
         )
     except Exception as e:
-        error_log = frappe.log_error(message=frappe.get_traceback(), title=str(e))
+        error_log = frappe.log_error(
+            message=frappe.get_traceback(), title=str(e))
         frappe.throw(
             _(
                 "Error in Notification: {}".format(
